3114c91add95f7ac66debdfa6e32529a1fee3519
angadv12 <angadbrar@outlook.com>
2025-01-30 16:51:47 -0500

    react native app scaffold set up with login functionality (sign out doesn't redirect back to login/default screen but signs out), firebase auth setup and working

---
+++ divvy_bill_app/app/(tabs)/index.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { auth } from '../../FirebaseConfig';
+import { getAuth } from '@firebase/auth';
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+
+export default function TabOneScreen() {
+
+    useEffect(() => {
+      const sign_out = getAuth().onAuthStateChanged(async (user) => {
+        if (!user) {
+          console.log('User is signed out');
+          router.replace('/')
+        }
+      });
+      return () => {
+        sign_out();
+      }
+    }, []);
+    
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Out</Text>
+      <TouchableOpacity onPress={() => auth.signOut()}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
